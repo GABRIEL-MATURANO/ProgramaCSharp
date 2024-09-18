e77672ebf21c92b1eb48a9c083b5cdc484df7a11
Gabriel Maturano <gabipirata2015@gmail.com>
2024-09-18 00:24:01 -0300

    Agregar archivos de proyecto.

---
+++ Entrega.FE/Contacto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega.FE
+{
+    public class Contacto
+    {
+        public string nombre {  get; set; }
+        public string telefono { get; set; }
+
+        public override string ToString()
+        {
+            return nombre.ToUpper() + "-" + telefono;
+        }
+
+        
+    }
+}
